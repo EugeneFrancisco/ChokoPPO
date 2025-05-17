ea039ea755371be8985a84870a0da1d22980eb37
Eugene Francisco <eugjavfra01@gmail.com>
2025-05-17 15:34:34 -0700

    tests added for step. Added utility for row, col to action

--- envs/choko_env.py
+++ envs/choko_env.py
@@ -20,27 +20,32 @@ class Choko_Env:
         return self.board
     
     def step(self, action):
+        # TODO change drop initiative
         assert self.action_space.contains(action), f"Invalid action {action}"
 
         # First 25 moves are for placing a piece
         if action < 25:
             row, col = divmod(action, 5)
             if self.board[row, col] == 0:
-                self.board[row, col] = 1
+                self.board[row, col] = self.player
             else:
                 raise ValueError("Invalid move: Cell already occupied")
         
         # next 48 moves are for moving a piece
         elif action < 125:
+            # TODO implement board sum
+            if self.drop_initiative != self.player:
+                raise ValueError("Invalid move: Not your drop initiative")
             action -= 25
             index, direction = divmod(action, 4)
-            row, col, _, _ = divmod(index, 5)
+            row, col = divmod(index, 5)
             
-            next_row, next_col = self.check_valid_move(row, col, direction)
+            next_row, next_col, _, _ = self.check_valid_move(row, col, direction)
             self.board[next_row, next_col] = self.player
             self.board[row, col] = 0
         
         else:
+            # TODO, implement board sum
             action -= 125
             jump_index, capture_index = divmod(action, 25)
             capture_row, capture_col = divmod(capture_index, 5)
@@ -57,15 +62,31 @@ class Choko_Env:
             self.board[between_row, between_col] = 0
 
             # extra capture
-            if self.board[capture_row, capture_col] == 0:
-                raise ValueError("Invalid move: capturing empty space")
             if self.board[capture_row, capture_col] == self.player:
                 raise ValueError("Invalid move: Cannot capture your own piece")
             
             self.board[capture_row, capture_col] = 0
+        
+        # TODO delete this after debugging
+        # self.player = 3 - self.player
+        # TODO: return the flattened observations.
+            
+    def board_sum(self):
+        return self.board[self.board == self.player].sum()
+    
+    @staticmethod
+    def to_action(row, col, move_type, direction=None, capture_row = None, capture_col = None):
+        if move_type == "place":
+            return row * 5 + col
+        if move_type == "move":
+            return 25 + (row*5 + col) * 4 + direction
+        if move_type == "jump":
+            if capture_row is None or capture_col is None:
+                raise ValueError("capture_row and capture_col must be provided for jump")
+            jump_index = (row*5 + col) * 4 + direction
+            capture_index = capture_row * 5 + capture_col
+            return 125 + jump_index * 25 + capture_index
 
-            
-            
     def check_valid_move(self, row, col, direction, jump=False):
         if self.board[row, col] != self.player:
             raise ValueError("Invalid move: Not your piece")
@@ -109,3 +130,114 @@ class Choko_Env:
             raise ValueError("Invalid move: Cell already occupied")
 
         return next_row, next_col, between_row, between_col
+
+
+def run_tests():
+    env = Choko_Env()
+
+    # testing that we can place a piece
+    env.step(0)
+    assert env.board[0, 0] == 1
+
+    env.step(21)
+    row, col = divmod(21, 5)
+    assert env.board[row, col] == 1
+
+    row = 3
+    col = 2
+    action = env.to_action(row=row, col=col, move_type="place")
+    env.step(action)
+    assert env.board[row, col] == 1
+
+    # testing that we can move a piece
+    row = 0
+    col = 0
+    direction = 2
+    action = env.to_action(row=row, col=col, move_type="move", direction=direction)
+    next_row, next_col, _, _ = env.check_valid_move(row, col, direction)
+
+    env.step(action)
+    assert env.board[row, col] == 0
+    assert env.board[next_row, next_col] == 1
+
+    row = 3
+    col = 2
+    direction = 1
+    action = env.to_action(row=row, col=col, move_type="move", direction=direction)
+    assert env.board[row, col] == 1
+    next_row, next_col, _, _ = env.check_valid_move(row, col, direction)
+    env.step(action)
+    assert env.board[row, col] == 0
+    assert env.board[next_row, next_col] == 1
+
+    # testing that we can jump over a piece, no extra capture
+
+    env.reset()
+    first_row = 2
+    first_col = 2
+    action = env.to_action(row=first_row, col=first_col, move_type="place")
+    env.step(action)
+    assert env.board[first_row, first_col] == 1
+
+    env.player = 2
+    second_row = 2
+    second_col = 3
+    action = env.to_action(row=second_row, col=second_col, move_type="place")
+    env.step(action)
+    assert env.board[second_row, second_col] == 2
+
+    action = env.to_action(
+        row=first_row,
+        col=first_col,
+        move_type="jump",
+        direction=1,
+        capture_row=second_row,
+        capture_col=second_col
+    )
+
+    env.player = 1
+    next_row, next_col, between_row, between_col = env.check_valid_move(first_row, first_col, 1, jump=True)
+    env.step(action)
+
+    assert env.board[first_row, first_col] == 0
+    assert env.board[next_row, next_col] == 1
+    assert env.board[between_row, between_col] == 0
+
+    # testing that we can jump over a piece, with extra capture
+
+    env.reset()
+    first_row = 2
+    first_col = 2
+    action = env.to_action(row=first_row, col=first_col, move_type="place")
+    env.step(action)
+    assert env.board[first_row, first_col] == 1
+
+    env.player = 2
+    second_row = 2
+    second_col = 3
+    action = env.to_action(row=second_row, col=second_col, move_type="place")
+    env.step(action)
+    assert env.board[second_row, second_col] == 2
+
+    action = env.to_action(
+        row=first_row,
+        col=first_col,
+        move_type="jump",
+        direction=1,
+        capture_row=second_row,
+        capture_col=second_col
+    )
+
+    env.player = 1
+    next_row, next_col, between_row, between_col = env.check_valid_move(first_row, first_col, 1, jump=True)
+    env.step(action)
+
+    assert env.board[first_row, first_col] == 0
+    assert env.board[next_row, next_col] == 1
+    assert env.board[between_row, between_col] == 0
+
+if __name__ == "__main__":
+    run_tests()
+
+
+
