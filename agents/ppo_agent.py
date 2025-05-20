0407539c91866f93a66442ba3882aa36723c4fc1
Eugene Francisco <eugjavfra01@gmail.com>
2025-05-19 20:00:12 -0700

    added PPO agent, forward, act, and critic methods all implemented

---
+++ agents/ppo_agent.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.distributions import Categorical
+
+NEG_INF = -1e10
+
+class PPOAgent(nn.Module):
+    def __init__(self, num_actions: int, hidden_dim: int = 64):
+        super().__init__()
+        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+        print(f"Using device: {self.device}")
+        self.hidden_dim = hidden_dim
+        self.num_actions = num_actions
+        self.trunk = nn.Sequential(
+            nn.Linear(25, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU()
+        )
+        self.actor_head = nn.Linear(self.hidden_dim, self.num_actions)
+        self.critic_head = nn.Linear(self.hidden_dim, 1)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=3e-4)
+        self.eps = 1e-8
+        self.trunk.to(self.device)
+        self.actor_head.to(self.device)
+        self.critic_head.to(self.device)
+    
+    def switch_to_cpu(self):
+        '''
+        Switches the model to CPU.
+        '''
+        if self.device.type == 'cpu':
+            pass
+        self.trunk.cpu()
+        self.actor_head.cpu()
+        self.critic_head.cpu()
+    
+    def switch_to_device(self):
+        if self.device.type != 'cpu':
+            pass
+        self.trunk.to(self.device)
+        self.actor_head.to(self.device)
+        self.critic_head.to(self.device)
+
+
+    
+    def forward(self, obs: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        trunk_out = self.trunk(obs)
+        action_logits = self.actor_head(trunk_out)
+        value = self.critic_head(trunk_out)
+        return action_logits, value
+    
+    def act(self, obs: torch.Tensor, mask: torch.Tensor) -> Categorical:
+        '''
+        Given an observation and a mask of the action space,
+        returns an action sampled from the policy.
+        Args:
+            obs: [1, 25] tensor of observations
+            mask: [1, 2625] tensor of action mask (note that 1 means keep and 0 means remove)
+        Returns:
+            action: int, the action sampled from the policy
+        '''
+        
+        trunk_out = self.trunk(obs)
+        action_logits = self.actor_head(trunk_out)
+        masked_logits = action_logits + (1 - mask) * NEG_INF
+        probs = torch.softmax(masked_logits, dim=-1)
+        dist = Categorical(probs)
+        return dist
+    
+    def critic(self, obs: torch.Tensor) -> torch.Tensor:
+        '''
+        Given a batch of observations, returns a batch of values.
+        Args:
+            obs: [batch_size, 25] tensor of observations
+        Returns:
+            value: [batch_size, 1] tensor of values
+        '''
+        trunk_out = self.trunk(obs)
+        value = self.critic_head(trunk_out)
+        return value.squeeze(1)
--- infrastructure/replay_buffer.py
+++ infrastructure/replay_buffer.py
@@ -1,5 +1,6 @@
 from envs.choko_env import Choko_Env
 from agents.random_agent import RandomAgent
+from agents.ppo_agent import PPOAgent
 from torch.utils.data import Dataset, DataLoader
 from . import utils
 import numpy as np
@@ -10,10 +11,11 @@ MAX_SIZE = 1000000
 NEG_INF = -1e10
 
 class RLDataset(Dataset):
-    def __init__(self, obs, actions, advantages, returns):
+    def __init__(self, obs, actions, logps, advantages, returns):
         self.obs = torch.from_numpy(obs).float()
         self.actions = torch.from_numpy(actions)
         self.advantages = torch.from_numpy(advantages).float()
+        self.logps = torch.from_numpy(logps).float()
         self.returns = torch.from_numpy(returns).float()
 
     
@@ -21,27 +23,24 @@ class RLDataset(Dataset):
         return len(self.obs)
     
     def __getitem__(self, idx):
-        return (self.obs[idx], self.actions[idx], self.advantages[idx], self.returns[idx])
+        return (self.obs[idx], self.actions[idx], self.logps[idx], self.advantages[idx], self.returns[idx])
 
 class ReplayBuffer:
-    # create a replay buffer that stores the MAX_SIZE transitions
-    # i.e., there are MAX_SIZE tuples of (s, a, r, s', d) in the buffer
-    def __init__(self, gamma, lam, max_size = MAX_SIZE):
+    def __init__(self, gamma, lam, ppo_agent, max_size = MAX_SIZE):
         self.gamma = gamma
         self.lam = lam
         self.max_size = max_size
         self.env = Choko_Env()
-        self.agent = RandomAgent()
-        self.critic = nn.Sequential(
-            nn.Linear(25, 1)
-        )
+        self.ppo_agent = ppo_agent
+        self.agent = self.ppo_agent.act
+        self.critic = self.ppo_agent.critic
     
     def make_dataloader(self, batch_size = 64, shuffle = True, num_workers = 0) -> DataLoader:
         '''
         Returns a DataLoader for the dataset
         '''
-        dataset_obs, actions, advantages, returns = self.make_dataset()
-        dataset = RLDataset(dataset_obs, actions, advantages, returns)
+        dataset_obs, actions, logps, advantages, returns = self.make_dataset()
+        dataset = RLDataset(dataset_obs, actions, logps, advantages, returns)
         dataloader = DataLoader(dataset, batch_size = batch_size, shuffle = shuffle, num_workers = num_workers)
         return dataloader
 
@@ -65,6 +64,7 @@ class ReplayBuffer:
         # combine all the transition tuples across all the rollouts together to make a dataset.
         dataset_obs = []
         actions = []
+        logps = []
         advantages = []
         returns = []
 
@@ -72,10 +72,12 @@ class ReplayBuffer:
             # sample a trajectory
             player_1_states = []
             player_1_actions = []
+            player_1_logps = []
             player_1_rewards = []
             
             player_2_states = []
             player_2_actions = []
+            player_2_logps = []
             player_2_rewards = []
 
             raw_obs, mask = self.env.reset()
@@ -91,18 +93,20 @@ class ReplayBuffer:
                         obs_torch = 3 - torch.from_numpy(obs).float().unsqueeze(0)
                         player_2_states.append(obs)
                     # (2) get the action and add the action
-                    logits = self.agent.forward(obs_torch) # note obs here is just one observation TODO, maybe change to torch?
+
                     mask = torch.from_numpy(mask).float().unsqueeze(0)
-                    assert logits.shape == mask.shape
-                    masked_logits = logits + (1 - mask) * NEG_INF
-                    probs = torch.softmax(masked_logits, dim = -1)
-                    dist = torch.distributions.Categorical(probs)
+                    dist = self.agent(obs_torch, mask)
                     action = dist.sample()
+
+                    logp = dist.log_prob(action)
+                    logp_value = logp.item()
 
                     if self.env.player == 1:
                         player_1_actions.append(action.item())
+                        player_1_logps.append(logp_value)
                     else:
                         player_2_actions.append(action.item())
+                        player_2_logps.append(logp_value)
 
                     state, reward, done, _ = self.env.step(action.item())
                     raw_obs, mask = state
@@ -126,15 +130,14 @@ class ReplayBuffer:
                                 player_1_rewards[-1] = -1
                         # add the last state to the trajectory
                         break
+                
                 player_1_states_np = np.stack(player_1_states, axis = 0)
                 player_1_values_tensor = self.critic(torch.as_tensor(player_1_states_np).float())
-                player_1_values_tensor = player_1_values_tensor.squeeze(1)
                 player_1_values = player_1_values_tensor.tolist()
                 player_1_values.append(0) # add the last value to the trajectory
                 
                 player_2_states_np = np.stack(player_2_states, axis = 0)
                 player_2_values_tensor = self.critic(torch.as_tensor(player_2_states_np).float())
-                player_2_values_tensor = player_2_values_tensor.squeeze(1)
                 player_2_values = player_2_values_tensor.tolist()
                 player_2_values.append(0)
 
@@ -149,8 +152,10 @@ class ReplayBuffer:
 
                 dataset_obs.extend(player_1_states)
                 actions.extend(player_1_actions)
+                logps.extend(player_1_logps)
                 advantages.extend(player_1_advantages)
                 returns.extend(player_1_returns)
+
 
 
                 player_2_returns, player_2_advantages = utils.compute_gae_and_returns(
@@ -162,27 +167,32 @@ class ReplayBuffer:
 
                 dataset_obs.extend(player_2_states)
                 actions.extend(player_2_actions)
+                logps.extend(player_2_logps)
                 advantages.extend(player_2_advantages)
                 returns.extend(player_2_returns)
             
         # convert the lists to numpy arrays
         dataset_obs = np.array(dataset_obs)
         actions = np.array(actions)
+        logps = np.array(logps)
         advantages = np.array(advantages)
         returns = np.array(returns)
 
-        return dataset_obs, actions, advantages, returns
+        return dataset_obs, actions, logps, advantages, returns
 
 
 
 if __name__ == "__main__":
     env = Choko_Env()
-    buffer = ReplayBuffer(0.99, 0.95, max_size = 5000)
+    ppo_agent = PPOAgent(env.action_space.n, hidden_dim = 64)
+    ppo_agent.switch_to_cpu()
+    buffer = ReplayBuffer(0.99, 0.95, ppo_agent=ppo_agent, max_size = 5000)
     dataloader = buffer.make_dataloader(batch_size = 64, shuffle = True, num_workers = 0)
     for batch in dataloader:
-        obs, actions, advantages, returns = batch
+        obs, actions, logps, advantages, returns = batch
         print(obs.shape)
         print(actions.shape)
+        print(logps.shape)
         print(advantages.shape)
         print(returns.shape)
 
