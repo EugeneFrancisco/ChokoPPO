ab0a28fbccfde1b43b3c9d8b9f7b5956c73c3498
Eugene Francisco <eugjavfra01@gmail.com>
2025-05-23 13:45:34 -0700

    added config file for magic numbers

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# training loop hyperparameters
+BATCH_SIZE     = 128
+ROLLOUT_LENGTH = 4096
+HIDDEN_DIM     = 64
+NUM_EPOCHS     = 6
+NUM_ITERATIONS = 2000
+
+# environment / action settings
+NUM_ACTIONS = (25) + (25 * 4) + (25 * 4 * 25)  # 2625 possible actions
+EVAL_ROUNDS = 100
+
+# PPO-specific coefficients
+C1      = 0.5
+C2      = 0.01
+EPS_CLIP = 0.2
+NEG_INF  = -1e10
+GAMMA = 0.99
+LAM = 0.95
